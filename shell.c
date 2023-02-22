f16b84ab85d5184ea3048913af37690e2a461bfd
David Oduse <odusedavid@gmail.com>
2023-02-21 17:24:57 -0800

    new solution

--- shell.c
+++ shell.c
@@ -1,29 +1,87 @@
-#include "shell.h"
-/**
- * main - the shell program start point
- * @ac: int num of command line args
- * @av: null term'd arr of strs contain args
- * @ev: null term'd arr of strs contain env vars
- * Return: int result. 0 in success, everything else is an error
- */
-int main(int ac, char **av, char **ev)
-{
-	sev_t sev;
-	int exitcode = 0;
-	(void)ac;
-	init_shell_env(&sev, av, ev);
+#include "holberton.h"
 
-	while (sev.skywalker)
+/**
+ * execute - executes the command
+ * @cmd: command to run
+ * Return: 0 on success1 -1 if cmd is exit and 1 on any other error
+ */
+int execute(char **cmd)
+{
+
+	pid_t child_pid;
+	int status;
+
+	if (strncmp("exit", cmd[0], 4) == 0)
+		return (-1);
+
+	child_pid = fork();
+
+	if (child_pid == -1)
 	{
-		dis_prompt(sev);
-		getcom(&sev);
-		checker_alias(&sev);
-		if (!check_builtin(&sev))
-			actions(&sev);
-		dis_error(&sev);
+		perror("Error");
+		return (1);
 	}
-	write_log(&sev);
-	exitcode = sev.error;
-	cl_sev(&sev);
-	return (exitcode);
+	else if (child_pid == 0)
+	{
+		if (execve(cmd[0], cmd, NULL) == -1)
+		{
+			perror("Error");
+			exit(-1);
+		}
+	}
+	else
+		wait(&status);
+
+	return (0);
+}
+
+
+/**
+ * main - main simple shell
+ * @argc: number of arguments
+ * @argv: list of command line arguments
+ * Return: Always 0, -1 on error.
+ */
+
+int main(int argc, char **argv)
+{
+
+	int response;
+	char **tokens;
+	size_t bufsize = BUFSIZ;
+	int isPipe = 0;
+	char *buffer;
+
+	if (argc >= 2)
+	{
+		/*TODO: Handle cases where there is no argument, only the command*/
+		if (execve(argv[1], argv, NULL) == -1)
+		{
+			perror("Error");
+			exit(-1);
+		}
+		return (0);
+	}
+
+	buffer = (char *)malloc(bufsize * sizeof(char));
+	if (buffer == NULL)
+	{
+		perror("Unable to allocate buffer");
+		exit(1);
+	}
+
+	do {
+		if (isatty(fileno(stdin)))
+		{
+			isPipe = 1;
+			_puts("cisfun#: ");
+		}
+
+		getline(&buffer, &bufsize, stdin);
+		buffer[_strlen(buffer) - 1] = '\0';
+		tokens = stringToTokens(buffer);
+		response = execute(tokens);
+	} while (isPipe && response != -1);
+
+	return (0);
 }
